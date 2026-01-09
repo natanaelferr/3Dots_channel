c0858b4652a808a64875d3cdd94c64ab90040e9c
Natanael Ferreira <49493704+natanaelferr@users.noreply.github.com>
2026-01-09 05:25:27 -0300

    Add files via upload
    InitialFolder for vampireSurvivors

---
+++ VampireSurvivors_Clone/Game1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace TerrariaClone;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Main _main;
+
+    private float deltaTime;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+
+        Window.IsBorderless = true;
+        Window.AllowUserResizing = true;
+        _graphics.IsFullScreen = false;
+        _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+        _graphics.ApplyChanges();
+        IsMouseVisible = true;
+
+        _main = new Main(this.Content, this, _graphics, this.Window);
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+        _main.Initialize();
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _main.LoadContent();
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+        _main.Update(deltaTime);
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.DarkBlue);
+
+        _main.Draw(_spriteBatch);
+
+        base.Draw(gameTime);
+    }
+}
